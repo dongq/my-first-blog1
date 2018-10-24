04e6d3ffaa66a7d95329c65f7e1ff97d45e78783
dkkim <dongqsarang@naver.com>
2018-10-24 16:42:01 +0900

    add comments

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from .models import Post
 from django.utils import timezone
 from django.http import Http404
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 def post_list(request):
@@ -81,3 +81,18 @@ def post_edit(request, pk):
         'form': form,
     })
 
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
